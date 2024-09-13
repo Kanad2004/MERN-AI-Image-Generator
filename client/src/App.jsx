f6b9871c86dff33911856693d37283a31801cec1
Kanad2004 <kanadkulkarni2013@gmail.com>
2024-09-13 17:56:47 +0530

    First commit

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import {BrowserRouter, Routes, Route , Link } from "react-router-dom";
+import {Logo} from './assets'
+
+const App = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
+
+export default App;
